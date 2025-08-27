9966df60ef6905ebbdc727897156fb9e915a35fe
cdgraybill <cdgraybill@gmail.com>
2025-08-26 22:17:09 -0500

    tests working again

--- Java-Spock/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Spock/src/main/java/com/gildedrose/GildedRose.java
@@ -35,27 +35,24 @@ class GildedRose {
                 }
             }
 
-            decreaseSellIn(item);
             makeQualityAdjustment(item);
         }
     }
 
-    private void decreaseSellIn(Item item) {
+    private void makeQualityAdjustment(Item item) {
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.decreaseSellInByOne();
         }
-    }
 
-    private void makeQualityAdjustment(Item item) {
         if (item.hasNotReachedQualityCap() && item.name.equals("Aged Brie")) {
             item.increaseQualityByOne();
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < 0 && item.hasNotReachedQualityCap()) {
                 item.increaseQualityByOne();
             }
         }
 
-        if (item.sellIn < 0) {
+        if (item.sellIn < 0 && !item.name.equals("Aged Brie")) {
             if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
